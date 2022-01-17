d58a3b1a17b8c328ea9ac58de3309015bd067d51
emanuel.developer82 <emanuel.developer82@gmail.com>
2022-01-17 11:44:06 -0300

    Ajustes para adicionar modelmapper;

--- src/main/java/br/com/msemail/MsEmailApplication.java
+++ src/main/java/br/com/msemail/MsEmailApplication.java
@@ -1,13 +1,20 @@
 package br.com.msemail;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsEmailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsEmailApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
