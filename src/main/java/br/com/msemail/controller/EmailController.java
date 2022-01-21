3ae9f28b63162c6f01b336eef8c2e4f1731ee00b
emanuel.developer82 <emanuel.developer82@gmail.com>
2022-01-21 10:51:15 -0300

    Adicionando o serviço de solicitação de cadastro;

--- src/main/java/br/com/msemail/controller/EmailController.java
+++ src/main/java/br/com/msemail/controller/EmailController.java
@@ -1,6 +1,7 @@
 package br.com.msemail.controller;
 
 import br.com.msemail.dto.EmailDTO;
+import br.com.msemail.dto.SolicitacaoDTO;
 import br.com.msemail.service.EmailService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,17 +27,29 @@ public class EmailController {
     private static final Logger log = LoggerFactory.getLogger(EmailController.class);
     private final EmailService service;
 
+//    @PostMapping("/sendEmail")
+//    public ResponseEntity<?> sendingEmail(@RequestBody @Valid EmailDTO emailDto) {
+//        return ResponseEntity.ok(service.sendEmail(emailDto));
+//    }
 
 
-    @PostMapping("/sendEmail")
-    public ResponseEntity<?> sendingEmail(@RequestBody @Valid EmailDTO emailDto) {
-//        return new ResponseEntity<>(emailDto, HttpStatus.CREATED);
-        return ResponseEntity.ok(service.sendEmail(emailDto));
+//    //TODO: Serviço para teste. Deve ser retirado.
+//    @PostMapping("/sendEmail")
+//    public ResponseEntity<?> sendingEmail() {
+//        log.info("Enviando e-mail para o solicitante.");
+//        EmailDTO emailDTO = new EmailDTO();
+//        emailDTO.setEmailFrom("teste@teste.com");
+//        //emailDTO.setEmailTo("jajajaja");
+//        emailDTO.setSubject("TESTE");
+//        emailDTO.setBody("dsnfjhbdjfbjdbf");
+//
+//        return service.sendEmail(emailDTO);
+//
+//    }
+
+    @PostMapping("/solicitacao")
+    public ResponseEntity<?> solicitacao(@RequestBody @Valid SolicitacaoDTO solicitacaoDTO) {
+        return service.solicitacao(solicitacaoDTO);
     }
-
-
-
-
-
 
 }
