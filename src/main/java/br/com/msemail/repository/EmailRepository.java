1395d71df166f7e1fec46f6eb3cab1edbb7079b5
emanuel.developer82 <emanuel.developer82@gmail.com>
2022-01-21 10:50:24 -0300

    adicionando consulta de busca de usuário por e-mail;

--- src/main/java/br/com/msemail/repository/EmailRepository.java
+++ src/main/java/br/com/msemail/repository/EmailRepository.java
@@ -2,7 +2,10 @@ package br.com.msemail.repository;
 
 import br.com.msemail.entity.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface EmailRepository extends JpaRepository<Email, Long> {
+
+    Optional<Email> findByEmailTo(String email);
 
 }
