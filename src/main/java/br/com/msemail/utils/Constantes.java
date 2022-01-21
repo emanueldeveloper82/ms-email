0ce5d4f748c47d644f14dab94906c6ffd8ba6d6e
emanuel.developer82 <emanuel.developer82@gmail.com>
2022-01-21 10:46:46 -0300

    Constantes;

---
+++ src/main/java/br/com/msemail/utils/Constantes.java
@@ -0,0 +1,12 @@
+package br.com.msemail.utils;
+
+public class Constantes {
+
+    public static final String EMAIL_ENVIADO_COM_SUCESSO = "Email enviado com sucesso. ";
+    public static final String ERRO_ENVIAR_EMAIL = "Erro ao enviar e-mail. ";
+    public static final String ERRO_AUTENTICACAO = "Erro de autenticação. ";
+    public static final String ERRO_INTERNO = "Erro interno. ";
+    public static final String CAMPO_EMAIL_VAZIO = "O campo e-mail está vazio. ";
+    public static final String SERVICO_USUARIO_INDISPONIVEL = "O serviço de usuário está indisponível. ";
+
+}
